aaf9686243154699c18172346890b70737365deb
Vii <prennings@gmail.com>
2015-02-03 18:12:24 +0100

    Added airport functionality! Big thanks to SC-Scripting for giving it!

---
+++ ingame/vliegveld.php
@@ -0,0 +1,64 @@
+<?php
+/*
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ */
+
+require_once('../init.php');
+
+$error = array();
+$form_error = '';
+
+$country = $dbCon->query('SELECT setting_value FROM settings WHERE setting_id = 4 LIMIT 1')->fetch_assoc();
+$countryArray = json_decode($country['setting_value'], true);
+
+$tpl->assign('countryArray', $countryArray);
+$tpl->assign('currentCountry', $countryArray[$userData['country_id']]);
+
+// Check if user is loggedin, if so no need to be here...
+if (LOGGEDIN == FALSE) { header('Location: ' . ROOT_URL . 'index.php'); }
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+	if($userData['cash'] < 250) {
+		$error[] = 'Een ticket kost &euro; 250,- cash';
+	}
+	if(empty($_POST['country']) OR !isset($_POST['country'])) {
+		$error[] = 'Selecteer een land om waar je heen wilt vliegen.';
+	} else {
+		if(!isset($countryArray[$_POST['country']])) {
+			$error[] = 'Dit land bestaat niet!';
+		} else {
+			if($userData['country_id'] == $_POST['country']) {
+				$error[] = 'Je bent al in ' . $countryArray[$_POST['country']] . '!';
+			}
+		}
+	}
+	if (count($error) > 0) {
+        foreach ($error as $item) {
+            $form_error .= '- ' . $item . '<br />';
+        }
+        $tpl->assign('form_error', $form_error);
+    } else {
+        $result = $dbCon->query(' UPDATE
+                                        users
+                                    SET
+                                        cash = (cash - 250),
+                                        country_id = "' . addslashes($_POST['country']). '"
+                                    WHERE
+                                        id= "'. $userData['id']. '"');
+        
+        $tpl->assign('currentCountry', $countryArray[$_POST['country']]);
+        $tpl->assign('success', 'Je betaald 250 en bent nu in '. $countryArray[$_POST['country']] .'!');
+    }
+}
+$tpl->display('ingame/vliegveld.tpl');
---
+++ templates/blue/ingame/vliegveld.tpl
@@ -0,0 +1,46 @@
+{include 'ingame/header.tpl'}
+    <div id="centercontent" style="margin-top: 7px;">
+			
+	<div class="titel">
+            <div class="titelicoon"> <img src="{$TEMPLATE_URL}images/icons/title-icon.png" alt="icoon" title="icoon"/> </div>
+            <div class="titeltekst">Vliegveld</div>
+        </div>
+        
+        {if isset($form_error)}
+        <div class="melding bad small icon">
+            {$form_error}
+        </div>
+        {elseif isset($success)}
+        <div class="melding good small icon">
+            {$success}
+        </div>
+        {elseif isset($info) AND $info|count_characters > 0}
+        <div class="melding info small icon">
+            {$info}
+        </div>
+        {/if}
+        
+        <div class="tekstvak">
+            <p>
+                     Welkom bij het vliegveld.<br />
+                Selecteer hieronder een land waarheen je naartoe wilt reizen.<br /> <br />
+                
+                Je bent nu in <b>{$currentCountry}</b>! <br />
+                <em>Je betaald 250,- per vlucht.</em>
+            </p>
+            <form method="post" action="">
+                <fieldset>
+                    <label>Land:</label>
+                        <select name="country" class="normal">
+                            <option value="">Selecteer</option>
+                            {foreach from=$countryArray key=country_id item=country_name}
+                                <option value="{$country_id}">{$country_name}</option>
+                            {/foreach}
+                        </select>
+                </fieldset>
+                <input class="button small good" name="submit" type="submit" value="Vliegen!">
+            </form>
+        </div>      
+        <div class="titelfooter"></div>     
+    </div>
+{include 'ingame/footer.tpl'}
